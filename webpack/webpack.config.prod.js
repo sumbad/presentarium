4f93764479ef19393436b221b045cc725efc5fbe
d.sumbaev <you@example.com>
2018-03-10 02:53:14 +0800

    remove uglifyjs-webpack-plugin and fix build

--- webpack/webpack.config.prod.js
+++ webpack/webpack.config.prod.js
@@ -1,22 +1,26 @@
 var path = require('path');
 var webpack = require('webpack');
-const UglifyJSPlugin = require('uglifyjs-webpack-plugin')
+// const UglifyJSPlugin = require('uglifyjs-webpack-plugin')
 
 
 
 module.exports = function (helper) {
   return {
-    devtool: '#source-map',
+    // devtool: '#source-map',
     plugins: [
       new webpack.DefinePlugin({
         'process.env': {
           NODE_ENV: '"production"'
         }
       }),
-      new UglifyJSPlugin(),
       new webpack.LoaderOptionsPlugin({
         minimize: true
       })
-    ]
+    ],
+    // optimization: {
+    //   minimizer: [
+    //     new UglifyJSPlugin()
+    //   ]
+    // }
   }
 }
